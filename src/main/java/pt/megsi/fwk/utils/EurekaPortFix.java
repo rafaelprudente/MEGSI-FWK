46be5d32b16b02c3e9cef0d343fd9d60e44d4496
Rafael R Prudente dos Santos <rafael.prudente.santos@gmail.com>
2025-11-23 00:26:28 +0000

    UPDATE

--- src/main/java/pt/megsi/fwk/utils/EurekaPortFix.java
+++ src/main/java/pt/megsi/fwk/utils/EurekaPortFix.java
@@ -1,19 +1,25 @@
 package pt.megsi.fwk.utils;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class EurekaPortFix implements ApplicationListener<WebServerInitializedEvent> {
+    private final EurekaInstanceConfigBean eurekaInstanceConfig;
 
     @Override
     public void onApplicationEvent(WebServerInitializedEvent event) {
         int port = event.getWebServer().getPort();
-        System.setProperty("eureka.instance.nonSecurePort", String.valueOf(port));
 
+        eurekaInstanceConfig.setNonSecurePort(port);
+        // Se você também usa HTTPS:
+        // eurekaInstanceConfig.setSecurePortEnabled(false);
         log.info("Eureka port fixed to {}", System.getProperty("eureka.instance.nonSecurePort"));
     }
 }
