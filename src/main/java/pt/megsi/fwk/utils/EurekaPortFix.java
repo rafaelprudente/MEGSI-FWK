c2cadd0521d18dd65f6e9e355dc07396f3a63581
Rafael R Prudente dos Santos <rafael.prudente.santos@gmail.com>
2025-11-23 00:08:43 +0000

    UPDATE

--- src/main/java/pt/megsi/fwk/utils/EurekaPortFix.java
+++ src/main/java/pt/megsi/fwk/utils/EurekaPortFix.java
@@ -1,9 +1,11 @@
 package pt.megsi.fwk.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.context.WebServerInitializedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class EurekaPortFix implements ApplicationListener<WebServerInitializedEvent> {
 
@@ -11,5 +13,7 @@ public class EurekaPortFix implements ApplicationListener<WebServerInitializedEv
     public void onApplicationEvent(WebServerInitializedEvent event) {
         int port = event.getWebServer().getPort();
         System.setProperty("eureka.instance.nonSecurePort", String.valueOf(port));
+
+        log.info("Eureka port fixed to {}", System.getProperty("eureka.instance.nonSecurePort"));
     }
 }
